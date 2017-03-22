064291bd6e90887a2b6c0bfe600dae0487775d67
dulhare1 <dulhare1+github@gmail.com>
2017-03-22 22:31:02 +0800

    How to interrupt the Run state?

--- RoutineKiller魯蛇殺手/RoutineKiller魯蛇殺手/Form1.Designer.cs
+++ RoutineKiller魯蛇殺手/RoutineKiller魯蛇殺手/Form1.Designer.cs
@@ -35,29 +35,32 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.nicon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Location = new System.Drawing.Point(12, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 15);
+            this.label1.Size = new System.Drawing.Size(138, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "按下Ctral+F1開始錄製";
+            this.label1.Text = "按下Alt+F1開始錄製";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(205, 75);
+            this.label2.Location = new System.Drawing.Point(12, 303);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(148, 15);
+            this.label2.Size = new System.Drawing.Size(138, 15);
             this.label2.TabIndex = 1;
-            this.label2.Text = "按下Ctral+F2開始執行";
+            this.label2.Text = "按下Alt+F2開始執行";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(188, 30);
+            this.textBox1.Location = new System.Drawing.Point(198, 236);
             this.textBox1.MaxLength = 5;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 25);
@@ -67,7 +70,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 34);
+            this.label3.Location = new System.Drawing.Point(12, 239);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(165, 15);
             this.label3.TabIndex = 3;
@@ -76,17 +79,47 @@
             // nicon
             // 
             this.nicon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.nicon.BalloonTipText = "Ctrl+F1結束錄製，Ctrl+F2結束執行";
+            this.nicon.BalloonTipText = "Alt+F1結束錄製，Alt+F2結束執行，Alt+Esc終止";
             this.nicon.BalloonTipTitle = "Tip";
             this.nicon.Icon = ((System.Drawing.Icon)(resources.GetObject("nicon.Icon")));
             this.nicon.Text = "nicon";
             this.nicon.Visible = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "按下Alt+Esc終止執行";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "輸入重複次數 :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(198, 271);
+            this.textBox2.MaxLength = 5;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(393, 374);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -106,6 +139,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NotifyIcon nicon;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- RoutineKiller魯蛇殺手/RoutineKiller魯蛇殺手/Form1.cs
+++ RoutineKiller魯蛇殺手/RoutineKiller魯蛇殺手/Form1.cs
@@ -17,29 +17,31 @@ namespace RoutineKiller魯蛇殺手
         {
             InitializeComponent();
         }
-        GlobalHotKey hotkey1, hotkey2;
+        GlobalHotKey hotkey_Record, hotkey_Run, hotkey_Stop;
 
         float screenFactor;
         Point CursorPos = new Point();
         List<object> ClickPriotity = new List<object>();
 
         //NotifyIcon nicon = new NotifyIcon();
-        bool isWorking = false;
-        bool isWorkingH3 = false;
+        bool hotkey_Record_isWorking = false;
+        bool hotkey_Run_isWorking = false;
+        bool stopSimulate = false;
+
         private void Form1_Load(object sender, EventArgs e)
         {
             screenFactor = ScreenAndMouse.ScreenAndMouseDefault.getScalingFactor();
 
-            hotkey1 = new GlobalHotKey(this.Handle, Keys.F1, Keys.Control); //註冊Control+F1為熱鍵(record)
-            hotkey1.OnHotkey += new GlobalHotKey.HotkeyEventHandler(hotkey1_OnHotkey);
+            hotkey_Stop = new GlobalHotKey(this.Handle, Keys.Escape,Keys.Alt);//註冊Alt+ESC為熱鍵(play)
+            hotkey_Stop.OnHotkey += new GlobalHotKey.HotkeyEventHandler(hotkey_Stop_OnHotkey);
             
-            hotkey2 = new GlobalHotKey(this.Handle, Keys.F2, Keys.Control);//註冊Control+F3為熱鍵(play)
-            hotkey2.OnHotkey += new GlobalHotKey.HotkeyEventHandler(hotkey2_OnHotkey);
+            hotkey_Record = new GlobalHotKey(this.Handle, Keys.F1, Keys.Alt); //註冊Alt+F1為熱鍵(record)
+            hotkey_Record.OnHotkey += new GlobalHotKey.HotkeyEventHandler(hotkey_Record_OnHotkey);
 
+            hotkey_Run = new GlobalHotKey(this.Handle, Keys.F2, Keys.Alt);//註冊Alt+F2為熱鍵(play)
+            hotkey_Run.OnHotkey += new GlobalHotKey.HotkeyEventHandler(hotkey_Run_OnHotkey);
             
-            //nicon.Icon = new Icon("Snake.ico");
             nicon.Text = "QuickMacro";
-            //this.Icon = nicon.Icon;
             nicon.Click += new EventHandler(nicon_Click);
         }
         void nicon_Click(object sender, EventArgs e)
@@ -49,10 +51,11 @@ namespace RoutineKiller魯蛇殺手
             Win32Native.Methods.SetForegroundWindow(this.Handle);
         }
 
-        private void hotkey1_OnHotkey(object sender, HotKeyEventArgs e) //Ctral + F2(record)
+        private void hotkey_Record_OnHotkey(object sender, HotKeyEventArgs e) //Alt + F2(record)
         {
-            isWorking = !isWorking; //按一次=true開始(+= new EventHandler),第二次=false關掉(-= new EventHandler)
-            if (isWorking)
+            if (hotkey_Run_isWorking) { return; }
+            hotkey_Record_isWorking = !hotkey_Record_isWorking; //按一次=true開始(+= new EventHandler),第二次=false關掉(-= new EventHandler)
+            if (hotkey_Record_isWorking==true)
             {
                 ClickPriotity.RemoveAll(it => true);
                 MessageBox.Show("開始錄製動作!\r\n本程式自動縮小。", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -99,8 +102,14 @@ namespace RoutineKiller魯蛇殺手
         }
 
 
-        private void hotkey2_OnHotkey(object sender, HotKeyEventArgs e) //Ctral + F2(run)
-        {
+        private void hotkey_Run_OnHotkey(object sender, HotKeyEventArgs e) //Alt + F2(run)
+        {       
+            if (hotkey_Record_isWorking) 
+            { 
+                MessageBox.Show("尚未終止錄製!\r\n請重新開啟程式錄製!!", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                System.Environment.Exit(System.Environment.ExitCode); //Can't Return!??
+                
+            }
             if (textBox1.TextLength == 0) 
             {
                 MessageBox.Show("請輸入執行間隔時間!", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -111,8 +120,14 @@ namespace RoutineKiller魯蛇殺手
                 MessageBox.Show("請先錄製動作!", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            isWorkingH3 = !isWorkingH3; //按一次=true開始(+= new EventHandler),第二次=false關掉(-= new EventHandler)
-            if (isWorkingH3 && ClickPriotity.Count != 0)
+            if(textBox2.TextLength==0)
+            {
+                MessageBox.Show("請輸入重複次數!", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            hotkey_Run_isWorking = !hotkey_Run_isWorking; //按一次=true開始(+= new EventHandler),第二次=false關掉(-= new EventHandler)         
+            if (hotkey_Run_isWorking)
             {
                 MessageBox.Show("開始鍵盤精靈!\r\n本程式自動縮小。", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 //---------------------------//
@@ -122,47 +137,64 @@ namespace RoutineKiller魯蛇殺手
 
                 simulateRecord();
                 MessageBox.Show("完成!!!", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                nicon_Click(this.nicon, new EventArgs());
             }
             else
             {
                 nicon_Click(this.nicon, new EventArgs());
             }
-            nicon_Click(this.nicon, new EventArgs());
+            
             
         }
 
         private void simulateRecord()
         {
-            
-            for (int i = 0; i < ClickPriotity.Count - 2; ) //取消(clrl+F2)會多算2次keydown,故減2
+            int sleepTime = Int16.Parse(textBox1.Text);
+            int times = Int16.Parse(textBox2.Text);
+            while (times != 0)
             {
-                Thread.Sleep(1000);
-                if (Object.ReferenceEquals(ClickPriotity[i + 1].GetType(), CursorPos.GetType()))
+                for (int i = 0; i < ClickPriotity.Count - 2; ) //錄製結束時(Alt+F1)會多2次keydown,故減2
                 {
-                    Cursor.Position = (Point)ClickPriotity[i + 1];
-                    Thread.Sleep(200);
-                    //textBox1.Text += "Mouse!" + "\r\n";
-                    if (WindwosHook.MouseButtons.Left.ToString() == ClickPriotity[i].ToString())
-                        SimulateControl.MouseControl.LeftClick();
-                    else if (WindwosHook.MouseButtons.Right.ToString() == ClickPriotity[i].ToString())
-                        SimulateControl.MouseControl.RightClick();
-                    i += 2;
-                }
-                else
-                {
-                    SimulateControl.KeyboardControl.KeyDown((Keys)ClickPriotity[i]);
-                    Thread.Sleep(30);
-                    SimulateControl.KeyboardControl.KeyUp((Keys)ClickPriotity[i]);
-                    i++;
-                }
+                    Thread.Sleep(sleepTime);
 
+                    if (stopSimulate == true) //(Alt+Esc)Can't shut down!???
+                        break;
+
+                    if (Object.ReferenceEquals(ClickPriotity[i + 1].GetType(), CursorPos.GetType()))
+                    {
+                        Cursor.Position = (Point)ClickPriotity[i + 1];
+                        Thread.Sleep(80);
+                        if (WindwosHook.MouseButtons.Left.ToString() == ClickPriotity[i].ToString())
+                            SimulateControl.MouseControl.LeftClick();
+                        else if (WindwosHook.MouseButtons.Right.ToString() == ClickPriotity[i].ToString())
+                            SimulateControl.MouseControl.RightClick();
+                        i += 2;
+                    }
+                    else
+                    {
+                        SimulateControl.KeyboardControl.KeyDown((Keys)ClickPriotity[i]);
+                        Thread.Sleep(30);
+                        SimulateControl.KeyboardControl.KeyUp((Keys)ClickPriotity[i]);
+                        i++;
+                    }
+                }
+                times--;
             }
         }
+
+        private void hotkey_Stop_OnHotkey(object sender, HotKeyEventArgs e) //(Alt+Esc)Can not shut down!??
+        { stopSimulate = true; this.Close(); System.Environment.Exit(System.Environment.ExitCode); }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((int)e.KeyChar < 48 | (int)e.KeyChar > 57 && (int)e.KeyChar!=8)
             { e.Handled = true; }
         }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar < 48 | (int)e.KeyChar > 57 && (int)e.KeyChar != 8)
+            { e.Handled = true; }
+        }
+
     }
 }
